7eca25ac69ea782f34ac148d73ebc89741fcc679
Eliezer de Almeida <buizzy@BuizzyMachine>
2020-01-06 14:16:19 -0300

    Criado arquivos bases para o aplicativo

---
+++ App.js
@@ -0,0 +1,30 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  View,
+} from 'react-native';
+
+
+
+
+export default class App extends Component {
+  render() {
+  return (
+    <View>
+      
+    </View>
+  );
+}
+}
+
+const style=StyleSheet.create({
+  
+}
+);
